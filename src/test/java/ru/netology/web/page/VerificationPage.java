e9da8b3456066d14ca103e193bfd898688a9dc3b
Grigoriy Kaygorodtsev <grigoriy.kaygorotsev@gmail.com>
2024-03-11 21:27:48 +0300

    Add artifacts

--- src/test/java/ru/netology/web/page/VerificationPage.java
+++ src/test/java/ru/netology/web/page/VerificationPage.java
@@ -1,4 +1,17 @@
 package ru.netology.web.page;
 
+import com.codeborne.selenide.SelenideElement;
+import static com.codeborne.selenide.Selenide.$;
+
+
 public class VerificationPage {
+    private SelenideElement codeField = $ ("[data-test-id=code] input");
+
+    private SelenideElement verefyButton = $("[data-test-id=action-verify]");
+
+    public VerificationPage() {
+
+
+
+    }
 }
