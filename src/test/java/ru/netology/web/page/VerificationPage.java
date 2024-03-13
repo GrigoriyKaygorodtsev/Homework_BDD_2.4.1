8bd711da2ffc59c67fb01eb2325dea25f5607fa1
Grigoriy Kaygorodtsev <grigoriy.kaygorotsev@gmail.com>
2024-03-13 19:35:18 +0300

    second commit

--- src/test/java/ru/netology/web/page/VerificationPage.java
+++ src/test/java/ru/netology/web/page/VerificationPage.java
@@ -1,17 +1,25 @@
 package ru.netology.web.page;
 
 import com.codeborne.selenide.SelenideElement;
+import ru.netology.web.data.DataHelper;
+
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
 
 public class VerificationPage {
-    private SelenideElement codeField = $ ("[data-test-id=code] input");
+    private final SelenideElement codeField = $ ("[data-test-id=code] input");
 
-    private SelenideElement verefyButton = $("[data-test-id=action-verify]");
+    private final SelenideElement verifyButton = $("[data-test-id=action-verify]");
 
     public VerificationPage() {
-
-
-
+        codeField.shouldBe(visible);
     }
+
+    public DashboardPage validVerify(DataHelper.VerificationCode verificationCode) {
+        codeField.setValue(verificationCode.getCode());
+        verifyButton.click();
+        return new DashboardPage();
+    }
+
 }
